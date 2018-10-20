3079a77bcb4caca0cb9f5730839377ad04aceb45
vinaypathak07 <pathak.vinay0001@gmail.com>
2018-10-20 23:03:01 +0530

    Cloning in Java

---
+++ CloningInJava/CloningConcept.java
@@ -0,0 +1,21 @@
+class Test implements Cloneable{
+	int i =10;
+	int j =20;
+
+	public static void main(String[] args) throws CloneNotSupportedException{
+
+			Test t1  = new Test();
+
+			//Test t2 = t1;
+
+		
+			Test t2 = (Test)t1.clone();
+	        
+	        t2.i =50 ;
+			t2.j =60 ;
+			
+			System.out.println(t1.i+"........."+t1.j);
+			System.out.println(t2.i+"........."+t2.j);
+
+	}
+}
