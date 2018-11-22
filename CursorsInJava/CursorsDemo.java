274e5fcc7a1162b21eb24ea846e2e6fce7bd342e
vinaypathak07 <pathak.vinay0001@gmail.com>
2018-11-22 23:43:22 +0530

    Cursors In Java
    Cursor In Java

---
+++ CursorsInJava/CursorsDemo.java
@@ -0,0 +1,16 @@
+// Important Loop Hole: Cursor(Enumeration(I),Iterator(I),ListIterator(I)) implemented class.
+import java.util.*;
+class CursorsDemo{
+	public static void main(String[] args){
+
+		Vector v =new Vector();
+
+		Enumeration e =v.elements();
+		Iterator itr =v.iterator();
+		ListIterator ltr=v.listIterator();
+
+		System.out.println(e.getClass().getName());
+		System.out.println(itr.getClass().getName());
+		System.out.println(ltr.getClass().getName());
+	}
+}
